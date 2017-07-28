f5df05ad56fc82098f91028f09046be5cd16eb46
shawn sandy <shawnsandy04@gmail.com>
2017-07-28 15:40:41 -0400

    update - add default roles/abilities to config array

--- src/config/config.php
+++ src/config/config.php
@@ -1,10 +1,21 @@
 <?php
 
 return [
-    'package' => [
-        'author' => "your name",
-        'email' => "your email",
-        'website' => "your website",
-        ]
+    'roles' => [
+        'superadmin' => "Super Admin",
+        'admin' => "Admin",
+        'staff' => "Staff",
+        'editor' => "Editor",
+        'member' => "Member",
+    ],
+
+    'abilities' => [
+        'assign_roles' => 'Assign Roles',
+        'manage_users' => 'Manage Users',
+        'manage_posts' => 'Manage Posts',
+        'manage_site' => 'Manage Site',
+        'manage_systems' => 'Manage Systems',
+    ]
+
 ];
 
