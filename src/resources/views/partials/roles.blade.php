e2451e4afa771252c471d467625984e013cf613c
shawn sandy <shawnsandy04@gmail.com>
2017-07-28 15:53:34 -0400

    Update - wip layout

--- src/resources/views/partials/roles.blade.php
+++ src/resources/views/partials/roles.blade.php
@@ -2,12 +2,16 @@
 @if(count($roles) && isset($user))
     <div class="roles">
         {{-- if user has role display add else display remove button--}}
+        <ul class="list-group">
         @foreach($roles as $role)
+            <li class="list-group-item">
             @if($user->isAn($role))
-                <a href="" class="btn btn-success">Remove Role</a>
+               <a href="" class="btn btn-link btn-block">Remove {{ $role }} Role</a>
             @else
-                <a href="" class="btn btn-success">Add Role</a>
+               <a href="" class="btn btn-link btn-block">Add {{ $role }} Role</a>
             @endif
+            </li>
         @endforeach
+        </ul>
     </div>
 @endif
