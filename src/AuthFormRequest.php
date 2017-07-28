cc25e03c2d28ef5aa037c6ceb83c2720930fc886
shawn sandy <shawnsandy04@gmail.com>
2017-07-28 15:38:55 -0400

    Feature - add Form request for auth methods

--- src/AuthFormRequest.php
+++ src/AuthFormRequest.php
@@ -9,16 +9,70 @@
 namespace ShawnSandy\DashAuth;
 
 
+use Auth;
 use Illuminate\Http\Request;
+use Silber\Bouncer\BouncerFacade as Bouncer;
 
 class AuthFormRequest extends Request
 {
 
-    public function save() {
-        $this->has("key");
-
-
+    public function authorize() {
+        return Auth::check();
     }
 
+    public function assignRoleToUser()
+    {
+
+        if ($this->has(["role", "user"])):
+
+            $user = $this->input("role");
+            $role = $this->input("user");
+
+            return Bouncer::assign($role)->to($user);
+        endif;
+
+        return false;
+    }
+
+    public function retractRoleFromUser()
+    {
+
+        if ($this->has(['user', 'role'])):
+
+            $user = $this->input("user");
+            $role = $this->input("role");
+            return Bouncer::retract($role)->from($user);
+
+        endif;
+
+        return false;
+    }
+
+
+    public function assignPrivilegeToRole()
+    {
+        if ($this->has(["role", "privilege"])):
+            $role = $this->input("role");
+            $privilege = $this->input("privilege");
+
+            return Bouncer::allow($role)->to($privilege);
+
+        endif;
+        return false;
+    }
+
+    public function removePrivilegeFromRole()
+    {
+        if ($this->has(['role', 'privilege'])):
+
+            $role = $this->input('role');
+            $privilege = $this->input("privilege");
+
+            return Bouncer::disallow($role)->to($privilege);
+
+        endif;
+        return false;
+
+    }
 
 }
