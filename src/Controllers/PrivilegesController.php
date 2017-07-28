200412b1f1137dc3349dac27515ec8b07284bc5a
shawn sandy <shawnsandy04@gmail.com>
2017-07-28 11:20:28 -0400

    Feature - add privileges   / roles controller

---
+++ src/Controllers/PrivilegesController.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shawnsandy
+ * Date: 7/28/17
+ * Time: 10:58 AM
+ */
+
+namespace ShawnSandy\DashAuth\Controllers;
+
+
+use ShawnSandy\DashAuth\AuthFormRequest;
+
+class PrivilegesController
+{
+
+    public function index() {
+        return "Hello world";
+    }
+
+
+    public function store(AuthFormRequest $request)
+    {
+
+    }
+
+    public function update(AuthFormRequest $request) {
+
+    }
+
+    public function destroy()
+    {
+
+    }
+
+
+}
---
+++ src/Controllers/RolesController.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shawnsandy
+ * Date: 7/28/17
+ * Time: 9:26 AM
+ */
+
+namespace ShawnSandy\DashAuth\Controllers;
+
+
+use Illuminate\Routing\Controller;
+
+class RolesController extends Controller
+{
+
+    public function index() {
+        return "Hello world";
+    }
+
+
+    public function store()
+    {
+
+    }
+
+    public function update() {
+
+    }
+
+    public function destroy()
+    {
+
+    }
+
+}
