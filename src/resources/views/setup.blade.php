e47f0fbf5bd872d5b839992a2116387ed544cc6f
Shawn Sandy <shawnsandy04@gmail.com>
2017-10-31 11:51:33 -0400

    update : add error and sucess notifications

--- src/resources/views/setup.blade.php
+++ src/resources/views/setup.blade.php
@@ -13,6 +13,7 @@
     .button {
         border-radius: 30px;
     }
+
 </style>
 
 <body>
@@ -20,28 +21,45 @@
     <section class="hero is-white is-fullheight has-text-centered">
         <div class="hero-body">
             <div class="container">
+            <div class="section has-text-centered">
+             @if(session('error'))
+                        <p class="notification is-danger">
+                            {{ session('error') }}
+                        </p>
+                        @endif
+                        @if(session('success'))
+                        <p class="notification is-success">
+                            {{ session('success') }}
+                        </p>
+                        @endif
+            </div>
 
                 <section class="section">
                     <div class="columns">
+
                         <div class="column is-6 is-offset-3">
 
                             <div class="">
-                            <div class="section">
-                                <div class="content">
-                                <h1 class="title is-1"><i class="fa fa-shield is-light" aria-hidden="true"></i></h1>
-                                    <h2 class="subtitle is-4 is-uppercase">
-                                         Dash Auth Setup
-                                    </h2>
-                                    <p class="is-6">
-                                    Dash Auth is simple way to create &amp manage Bouncer Roles &amp Abilities. Before we start you will need to create a super admin to manage roles &amp abilities. Please hit the button below to add  a super admin continue your site.
-                                    </p>
-                                    <div class="section">
-                                    <p>
-                                        <a href="/dashauth/setup" class="button is-info is-large is-uppercase is-outlined">Create Super Admin Now</a>
-                                    </p>
+                                <div class="section">
+                                    <div class="content">
+                                        <h1 class="title is-1">
+                                            <i class="fa fa-shield is-light" aria-hidden="true"></i>
+                                        </h1>
+                                        <h2 class="subtitle is-4 is-uppercase">
+                                            Dash Auth Setup
+                                        </h2>
+                                        <p class="is-6">
+                                            Dash Auth is simple way to create &amp manage Bouncer Roles &amp Abilities. Before we start you will need to create a super
+                                            admin to manage roles &amp abilities. Please hit the button below to add a super
+                                            admin continue your site.
+                                        </p>
+                                        <div class="section">
+                                            <p>
+                                                <a href="/dashauth/setup" class="button is-info is-large is-uppercase is-outlined">Create Super Admin Now</a>
+                                            </p>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
 
                             </div>
                         </div>
